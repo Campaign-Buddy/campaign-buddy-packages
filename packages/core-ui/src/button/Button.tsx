e067f1222aea8efbbb05922b1ac22f2c6df9f751
Joseph Stewart <joseph.s@nationalschoolproject.com>
2021-04-30 12:09:46 -0700

    Override interaction styles for button

--- packages/core-ui/src/button/Button.tsx
+++ packages/core-ui/src/button/Button.tsx
@@ -17,6 +17,22 @@ const StyledButton = styled(ButtonCore)`
 	& .bp3-icon {
 		color: ${({ theme }) => theme.colors.text} !important;
 	}
+	
+	&.bp3-minimal {
+		outline: none;
+	}
+
+	&.bp3-minimal:focus {
+		box-shadow: 0 0 0 1px #137cbd, 0 0 0 3px rgb(19 124 189 / 30%), inset 0 1px 1px rgb(16 22 26 / 20%);
+	}
+
+	&.bp3-minimal:hover {
+		background-color: rgba(228, 222, 210, .7);
+	}
+
+	&.bp3-minimal:active {
+		background-color: rgba(228, 222, 210, .9);
+	}
 `;
 
 StyledButton.defaultProps = {
