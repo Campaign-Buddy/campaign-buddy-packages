b0154ef22e85aaadea4d2fa265601011c44ece00
Joseph Stewart <joseph.william.stewart@biola.edu>
2021-12-08 21:55:33 -0800

    Fix navigation bug

--- packages/rich-text/src/keyBindings.ts
+++ packages/rich-text/src/keyBindings.ts
@@ -1,13 +1,31 @@
 import React from 'react';
-import { Editor, Transforms } from 'slate';
+import { Editor, Range, Transforms } from 'slate';
 
 type HotkeyHandler = (editor: Editor, event: React.KeyboardEvent) => void;
 
 export const keyBindings: Record<string, HotkeyHandler> = {
 	left: (editor) => {
+		if (!editor.selection) {
+			return;
+		}
+
+		if (!Range.isCollapsed(editor.selection)) {
+			Transforms.collapse(editor, { edge: 'start' });
+			return;
+		}
+
 		Transforms.move(editor, { unit: 'offset', reverse: true });
 	},
 	right: (editor) => {
+		if (!editor.selection) {
+			return;
+		}
+
+		if (!Range.isCollapsed(editor.selection)) {
+			Transforms.collapse(editor, { edge: 'end' });
+			return;
+		}
+
 		Transforms.move(editor, { unit: 'offset' });
 	},
 };
