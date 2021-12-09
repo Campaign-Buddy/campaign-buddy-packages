14d725e94858256c10bf7a30ad64937b8a097dd3
Joseph Stewart <joseph.william.stewart@biola.edu>
2021-12-08 21:17:03 -0800

    Don't allow empty links

--- packages/rich-text/src/withCampaignBuddyNodes.ts
+++ packages/rich-text/src/withCampaignBuddyNodes.ts
@@ -1,7 +1,9 @@
-import { Editor } from 'slate';
-import { voidNodes, inlineNodes } from './types';
+import { Editor, Element, Text, Transforms } from 'slate';
+import { voidNodes, inlineNodes, ElementNode } from './types';
 
 export function withCampaignBuddyNodes(editor: Editor): Editor {
+	const { normalizeNode } = editor;
+
 	editor.isVoid = (element) => {
 		return voidNodes.includes(element.kind);
 	};
@@ -10,5 +12,35 @@ export function withCampaignBuddyNodes(editor: Editor): Editor {
 		return inlineNodes.includes(element.kind);
 	};
 
+	editor.normalizeNode = (entry) => {
+		const [node, path] = entry;
+
+		// Link nodes must not be empty, it's confusing
+		if (
+			Element.isElement(node) &&
+			node.kind === 'link' &&
+			elementIsEmpty(node)
+		) {
+			Transforms.removeNodes(editor, { at: path });
+			return;
+		}
+
+		normalizeNode(entry);
+	};
+
 	return editor;
+}
+
+function elementIsEmpty(node: ElementNode): boolean {
+	return !node.children.some((child) => {
+		if (Text.isText(child)) {
+			return Boolean(child.text);
+		}
+
+		if (Element.isElement(child)) {
+			return elementIsEmpty(child);
+		}
+
+		return true;
+	});
 }
