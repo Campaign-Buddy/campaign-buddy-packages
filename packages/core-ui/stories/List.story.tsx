a9a5a3a8c16e40be5af4a5a63355d275bc0eed1f
Joseph Stewart <joseph.william.stewart@biola.edu>
2022-07-31 14:40:29 -0700

    Update list example to show kebab menu.

--- packages/core-ui/stories/List.story.tsx
+++ packages/core-ui/stories/List.story.tsx
@@ -1,30 +1,56 @@
 import React from 'react';
-import { List, ListItem, ListItemText } from '../src';
-import { ListItemIcon, ListItemIconButton } from '../src/list/List';
+import { useBooleanState } from '@campaign-buddy/common-hooks';
+import {
+	List,
+	ListItem,
+	ListItemText,
+	ListItemIcon,
+	ListItemIconButton,
+	MenuPopover,
+} from '../src';
 
 export default {
 	title: 'core-ui/List',
 };
 
-export const Primary = () => (
-	<List>
-		<ListItem onClick={() => console.log('bread :p')}>
-			<ListItemIcon icon="bold" />
-			<ListItemText text="Bread" />
-			<ListItemIcon icon="tick" />
-		</ListItem>
-		<ListItem onClick={() => console.log('milk :p')}>
-			<ListItemIcon icon="database" />
-			<ListItemText text="Milk" />
-		</ListItem>
-		<ListItem onClick={() => console.log('cheese :p')}>
-			<ListItemIcon icon="blank" />
-			<ListItemText text="Cheese" />
-		</ListItem>
-		<ListItem onClick={() => console.log('???')}>
-			<ListItemIcon icon="blank" />
-			<ListItemText text="Super duper duper duper duper duper duper long" />
-			<ListItemIconButton icon="menu" onClick={() => console.log('ICON!')} />
-		</ListItem>
-	</List>
-);
+export const Primary = () => {
+	const [isMenuOpen, openMenu, closeMenu] = useBooleanState(false);
+
+	return (
+		<List>
+			<ListItem onClick={() => console.log('bread :p')}>
+				<ListItemIcon icon="bold" />
+				<ListItemText text="Bread" />
+				<ListItemIcon icon="tick" />
+			</ListItem>
+			<ListItem onClick={() => console.log('milk :p')}>
+				<ListItemIcon icon="database" />
+				<ListItemText text="Milk" />
+			</ListItem>
+			<ListItem onClick={() => console.log('cheese :p')}>
+				<ListItemIcon icon="blank" />
+				<ListItemText text="Cheese" />
+				<MenuPopover
+					items={[
+						{
+							displayText: 'Delete',
+							icon: 'trash',
+						},
+						{
+							displayText: 'Rename',
+							icon: 'edit',
+						},
+					]}
+					isOpen={isMenuOpen}
+					onClose={closeMenu}
+				>
+					<ListItemIconButton icon="menu" onClick={openMenu} />
+				</MenuPopover>
+			</ListItem>
+			<ListItem onClick={() => console.log('???')}>
+				<ListItemIcon icon="blank" />
+				<ListItemText text="Super duper duper duper duper duper duper long" />
+			</ListItem>
+		</List>
+	);
+};
