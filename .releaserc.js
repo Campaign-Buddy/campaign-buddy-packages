6cd8a97a1c2ce16b393fc4a8f58c0bed243677a4
Joseph Stewart <joseph.william.stewart@biola.edu>
2023-04-13 20:50:34 -0700

    chore: remove per-package versioning.

--- .releaserc.js
+++ .releaserc.js
@@ -1,4 +1,8 @@
 module.exports = {
 	branches: ['master'],
-	extends: 'semantic-release-monorepo'
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		'@semantic-release/git',
+	],
 };
